ecc379f67b01e78a9c39fd11b003db3a30621199
CHANDAN SINGH <singh2002chandan@gmail.com>
2026-04-21 07:19:16 +0000

    datatypes

---
+++ .devcontainer/basics1/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log( 3+
+    3) //code readability should be high
+
+    let name = "chandan"
+    let age =  24
+    let isLoggedIn = false
+
+    // number =>2 to the power 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undifined =>
+    // symbol 
+
+    console.log(typeof undefined)
+    console.log(typeof null)
+    console.log(name, age, isLoggedIn)
