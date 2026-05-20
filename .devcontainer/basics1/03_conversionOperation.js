c7515f4323a00fad6c547380f0e0fa0a4fab58c2
CHANDAN SINGH <singh2002chandan@gmail.com>
2026-05-20 16:54:14 +0000

    datatypes

--- .devcontainer/basics1/03_conversionOperation.js
+++ .devcontainer/basics1/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33abc"
+/*let score = "33abc"
 
 console.log(typeof score)
 console.log(typeof(score))
@@ -24,4 +24,27 @@ console.log(booleanisloggedin)
 let someNumber = 33 
 let stringNumber = String(someNumber)
 console.log(stringNumber)
-console.log(typeof stringNumber)
+console.log(typeof stringNumber)*/
+
+//****************** operations***************************** */
+
+let value = 3
+let negValue = -value
+//console.log(negvalue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);*/
+
+let str1 = "chandan"
+let str2 = "singh"
+
+let str3 = str1+str2
+console.log(str3)
+
+console.log (2 + 3 + "1")
+console.log(2 + "2")
+console.log("3" + 2 +1)
---
+++ .devcontainer/basics1/datatypes-summary.js
@@ -0,0 +1,43 @@
+// primitive
+
+// 7 types: String, Number, Boolearn, null, ubdefined, Symbol,
+   
+
+
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsidetemp = null 
+let userEmail
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log (id===anotherid);
+
+const bignumber = 123212343564564456754
+
+console.log (typeof(bignumber))
+
+
+// non primitive
+
+//array, objects, functions
+
+
+//array
+const heros = ["ironman", "caption", "hulk"];
+
+//object
+let myObj= {
+    name: "chandan",
+    age: 24
+}
+
+//function
+
+const myFunction= function(){
+ console.log("Hello world")
+}
