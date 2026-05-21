265e54cb055f484ba887f6327e4c5b6c43f22cba
CHANDAN SINGH <singh2002chandan@gmail.com>
2026-05-21 17:24:09 +0000

    num and math

---
+++ .devcontainer/basics1/05_nums_and_math.js
@@ -0,0 +1,26 @@
+const score = 200 
+//console.log(score) // 200
+
+const num = new Number(100);
+//console.log(num) // [Number: 100]
+
+//console.log(num.toString().length); // 3 (converts the number to a string and then gets its length)
+//console.log(num.toFixed(1));// 100.00 (formats the number to 2 decimal places)
+
+const newNumber = 124.554;
+//console.log(newNumber.toPrecision(4)); // 124.6 (formats the number to 4 significant digits)
+
+//****************************math*******************************
+
+//console.log(Math);
+//console.log(Math.abs(-5)); // 5 (returns the absolute value of a number)
+//console.log(Math.round(4.7)); // 5 (rounds a number to the nearest integer)
+//console.log(Math.floor(4.7)); // 4 (rounds a number down to the nearest integer)
+//console.log(Math.ceil(4.2)); // 5 (rounds a number up to the nearest integer)
+//console.log(Math.max(1, 5, 3)); // 5 (returns the largest of the given numbers)
+//console.log(Math.min(1, 5, 3)); // 1 (returns the smallest of the given numbers)
+
+
+console.log(Math.random()); // (returns a random number between 0 and 1)
+console.log(Math.random() *10 +1);
+console.log(Math.sqrt(16)); // 4 (returns the square root of a number)
