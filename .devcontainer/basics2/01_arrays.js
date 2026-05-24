b58e7484517e141157b1e8580d6cccfea217441f
CHANDAN SINGH <singh2002chandan@gmail.com>
2026-05-24 16:05:11 +0000

    arrays

---
+++ .devcontainer/basics2/01_arrays.js
@@ -0,0 +1,28 @@
+// array
+
+const myarray = [0,1,2,3,4,5,6];
+const myheroes = ["superman", "batman", "spiderman"];
+
+const myarray2 = new Array(7,8,9);  
+//console.log(myarray[3]);
+
+//array methods
+
+// myarray.push(8);
+// console.log(myarray);
+
+// myarray.pop();
+// console.log(myarray);
+
+
+// myarray.unshift(6);
+// myarray.shift();
+// console.log(myarray)
+
+//slice , splice
+
+// const myarray3 = myarray.slice(2,5);
+// console.log(myarray3);
+
+const myarray4 = myarray.splice(2,2);
+console.log(myarray4);
---
+++ .devcontainer/basics2/02_arrays.js
@@ -0,0 +1,7 @@
+const marvelheroes = ["Iron Man", "Captain America", "Thor"];
+const dcheroes = ["Batman", "Superman"];
+
+marvelheroes.push(dcheroes);
+
+console.log(marvelheroes);
+console.log(marvelheroes[3][1]);
