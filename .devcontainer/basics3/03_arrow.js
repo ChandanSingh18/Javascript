4dc02de5a535d63a8b3ac6220e46ea55d62713cb
CHANDAN SINGH <singh2002chandan@gmail.com>
2026-05-29 09:33:13 +0000

    arrow function

--- .devcontainer/basics3/03_arrow.js
+++ .devcontainer/basics3/03_arrow.js
@@ -3,11 +3,36 @@ const user = {
     price: 1000,
 
     welcomeMessage : function() {
-        console.log(`the price is ${user.price}  for our website`);
-        console.log(this);
+        // console.log(`the price is ${user.price}  for our website`);
+        // console.log(this);
     }
 }
 
-user.welcomeMessage();
-user.username = "singh";
-user.welcomeMessage();
+// user.welcomeMessage();
+// user.username = "singh";
+// user.welcomeMessage();
+
+//console.log(this);
+
+// function welcome(){
+//     let username = "hitesh"
+//     console.log(username);
+// }
+// welcome()
+
+// const chai = () => {
+//      let username = "hitesh"
+//      console.log(this);
+// }
+// chai()
+
+// const addtwo = (num1, num2) => {
+//     return num1 + num2; //explicit return
+// }
+// console.log(addtwo(4,5));
+
+// const addtwo = (num1, num2) => (num1*num2);// implicit return
+// console.log(addtwo(6,6));
+
+const addtwo = (num1, num2) => ({username : "chandan",})
+console.log(addtwo(4, 5));
