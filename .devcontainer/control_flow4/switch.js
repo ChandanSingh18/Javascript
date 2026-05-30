c2ad6accbcb07b3807acbf97a7fa5ef43f4adf31
CHANDAN SINGH <singh2002chandan@gmail.com>
2026-05-30 14:36:42 +0000

    control flow

---
+++ .devcontainer/control_flow4/switch.js
@@ -0,0 +1,16 @@
+const day = "monday";
+
+switch (day) {
+    case "monday":
+        console.log("tuesday");
+        break;
+    case "wednesday":
+        console.log("wednesday");
+        break;
+    case "tuesday":
+        console.log("monday");
+        break;
+    default:
+        console.log("invalid day");
+
+}
