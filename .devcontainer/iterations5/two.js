94c653b9ab5db5f6d82973ef50412e88d19cf7c4
CHANDAN SINGH <singh2002chandan@gmail.com>
2026-06-03 11:09:01 +0000

    loops

---
+++ .devcontainer/iterations5/two.js
@@ -0,0 +1,23 @@
+// let index = 0
+// while (index<=10) {
+//     console.log(`value of index is: ${index}`);
+//     index++;
+    
+// }
+
+// let myarray = ["flash", "superman", "batman"];
+
+// let index = 0;
+// while(index<myarray.length){
+//     console.log(`value of index is:${myarray[index]}`);
+//     index++;
+// }
+
+//do while loop
+
+let score = 11;
+do{
+    console.log(`score is ${score}`)
+    score++;
+}
+while(score<=10);
