1d540f12020ea6508d7ea331047e2d07f8d94b88
CHANDAN SINGH <singh2002chandan@gmail.com>
2026-06-06 16:00:02 +0000

    loop

--- .devcontainer/iterations5/five.js
+++ .devcontainer/iterations5/five.js
@@ -5,6 +5,35 @@ const coding = ["js", "cpp", "python", "java"]
 // })
 
 coding.forEach((item) => {
-    console.log(item);
+    // console.log(item);
 })
 
+
+// coding.forEach(printMe)
+
+coding.forEach((item) => {
+    // console.log(item);
+})
+
+coding.forEach((item, index, arr) => {
+    // console.log(item,index, arr);
+})
+
+const mycoding=[
+    {
+        LanguageName: "javascript",
+        LanguageFileName: "js"
+    },
+    {
+        LanguageName: "java",
+        LanguageFileName: "java"
+    },
+    {
+        LanguageName: "python",
+        LanguageFileName: "py"
+    }
+]
+
+mycoding.forEach((item) => {
+    console.log(item.LanguageFileName);
+})
